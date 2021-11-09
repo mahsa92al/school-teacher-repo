3ec673f26eab84a0ae6ae57827c3930e7c9d49f6
Mahsa <mahsa.alikhany@gmail.com>
2021-11-09 08:45:23 +0330

    added teachers list to TeacherService

--- src/com/school/TeacherService.java
+++ src/com/school/TeacherService.java
@@ -1,7 +1,19 @@
 package com.school;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class TeacherService {
+    List<Teacher> teachers = new ArrayList<>();
+
+    public TeacherService() {
+        teachers.add(new FullTimeTeacher("mahla", "hedayat", "123", 6000000));
+        teachers.add(new FullTimeTeacher("ali", "akbari", "124", 4000000));
+        teachers.add(new PartTimeTeacher("sahar", "kamali", "234", 15000, 576));
+        teachers.add(new PartTimeTeacher("mohammad", "moradi", "235", 20000, 400));
+    }
+
 }
