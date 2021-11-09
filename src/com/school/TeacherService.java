b9686c361e2b70b928bb67f949bfe4bebe581480
Mahsa <mahsa.alikhany@gmail.com>
2021-11-09 11:21:44 +0330

    added addSchoolByPersonalCode to TeacherService

--- src/com/school/TeacherService.java
+++ src/com/school/TeacherService.java
@@ -1,7 +1,9 @@
 package com.school;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -14,6 +16,26 @@ public class TeacherService {
         teachers.add(new FullTimeTeacher("ali", "akbari", "124", 4000000));
         teachers.add(new PartTimeTeacher("sahar", "kamali", "234", 15000, 576));
         teachers.add(new PartTimeTeacher("mohammad", "moradi", "235", 20000, 400));
+    }
+
+    public Optional<Teacher> findByPersonalCode(String personalCode){
+        return teachers.stream().filter(i-> i.getPersonalNumber().equals(personalCode)).findAny();
+    }
+
+    public Teacher addSchoolByPersonalCode(String personalCode, School school){
+        Optional<Teacher> foundTeacher = findByPersonalCode(personalCode);
+        if(!foundTeacher.isPresent()){
+            throw new RuntimeException("the teacher not found");
+        }
+        if(foundTeacher.get().getSchool() == null){
+            foundTeacher.get().setSchool(new HashSet<>());
+        }
+        if(foundTeacher.get().getSchool().contains(school)){
+            throw new RuntimeException("duplicate school!");
+        }
+        foundTeacher.get().getSchool().add(school);
+
+        return foundTeacher.get();
     }
 
 }
