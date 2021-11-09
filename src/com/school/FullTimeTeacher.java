ebaf9b5892f90787f8c2cfd887bac80aefbbc6a6
Mahsa <mahsa.alikhany@gmail.com>
2021-11-09 14:07:30 +0330

    added degree and type to fulltimeTescher constructor

--- src/com/school/FullTimeTeacher.java
+++ src/com/school/FullTimeTeacher.java
@@ -1,10 +1,13 @@
 package com.school;
 
+import com.school.enums.Degree;
+import com.school.enums.TeacherType;
+
 public class FullTimeTeacher extends Teacher {
     private double baseSalary;
 
-    public FullTimeTeacher(String name, String lastName, String personalCode, double baseSalary) {
-        super(name, lastName, personalCode);
+    public FullTimeTeacher(String name, String lastName, String personalCode, Degree degree, TeacherType type, double baseSalary) {
+        super(name, lastName, personalCode, degree, type);
         this.baseSalary= baseSalary;
     }
 
