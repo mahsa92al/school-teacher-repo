fe98dbe80451915f7e2be682e56532d2da64612c
Mahsa <mahsa.alikhany@gmail.com>
2021-11-09 16:15:02 +0330

    added experienceYear to follTimeTeacher constructor

--- src/com/school/FullTimeTeacher.java
+++ src/com/school/FullTimeTeacher.java
@@ -6,8 +6,8 @@ import com.school.enums.TeacherType;
 public class FullTimeTeacher extends Teacher {
     private double baseSalary;
 
-    public FullTimeTeacher(String name, String lastName, String personalCode, Degree degree, TeacherType type, double baseSalary) {
-        super(name, lastName, personalCode, degree, type);
+    public FullTimeTeacher(String name, String lastName, String personalCode, Degree degree, TeacherType type, Integer experienceYear, double baseSalary) {
+        super(name, lastName, personalCode, degree, type, experienceYear);
         this.baseSalary= baseSalary;
     }
 
